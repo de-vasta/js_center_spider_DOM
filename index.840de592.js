af955161ff9aefe24d60a5a88bfbfa996dc95fbb
Nazar Kundys <nazarrisd@gmail.com>
2025-12-31 19:52:11 +0200

    make build

---
+++ index.840de592.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.querySelector(".spider");t.style.top=(e.clientHeight-t.clientHeight)/2+"px",t.style.left=(e.clientWidth-t.clientWidth)/2+"px";
+//# sourceMappingURL=index.840de592.js.map
