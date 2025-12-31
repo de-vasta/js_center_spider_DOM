82db263197ee378d83ce3135253af05176e67fb0
Nazar Kundys <nazarrisd@gmail.com>
2025-12-31 19:45:48 +0200

    ♻️ refactor(spider): improve centering calculation using scroll dimensions
    - Replace CSS-based centering with JavaScript positioning
    - Calculate center using `scrollHeight` and `scrollWidth` properties

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const spider = document.querySelector('.wall > .spider');
+const wall = document.querySelector('.wall');
+const spider = wall.querySelector('.spider');
 
-spider.style.inset = '0';
-spider.style.margin = 'auto';
+spider.style.top = (wall.scrollHeight - spider.scrollHeight) / 2 + 'px';
+
+spider.style.left = (wall.scrollWidth - spider.scrollWidth) / 2 + 'px';
